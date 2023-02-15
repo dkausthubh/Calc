17f2f2c5bf708d4d1b106d1684f49a9f0b257cd8
Kausthubh Dadhich <dkausthubh@outlook.com>
2023-02-15 12:53:33 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.N1 = new System.Windows.Forms.Button();
+            this.N4 = new System.Windows.Forms.Button();
+            this.N2 = new System.Windows.Forms.Button();
+            this.N3 = new System.Windows.Forms.Button();
+            this.N5 = new System.Windows.Forms.Button();
+            this.N6 = new System.Windows.Forms.Button();
+            this.N7 = new System.Windows.Forms.Button();
+            this.N8 = new System.Windows.Forms.Button();
+            this.N9 = new System.Windows.Forms.Button();
+            this.N0 = new System.Windows.Forms.Button();
+            this.bc = new System.Windows.Forms.Button();
+            this.bdec = new System.Windows.Forms.Button();
+            this.bad = new System.Windows.Forms.Button();
+            this.bsub = new System.Windows.Forms.Button();
+            this.bmult = new System.Windows.Forms.Button();
+            this.bdiv = new System.Windows.Forms.Button();
+            this.bequal = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(14, 27);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(274, 31);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // N1
+            // 
+            this.N1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.N1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.N1.Location = new System.Drawing.Point(29, 87);
+            this.N1.Name = "N1";
+            this.N1.Size = new System.Drawing.Size(41, 30);
+            this.N1.TabIndex = 1;
+            this.N1.Text = "1";
+            this.N1.UseVisualStyleBackColor = false;
+            this.N1.Click += new System.EventHandler(this.N1_Click);
+            // 
+            // N4
+            // 
+            this.N4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.N4.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.N4.Location = new System.Drawing.Point(29, 123);
+            this.N4.Name = "N4";
+            this.N4.Size = new System.Drawing.Size(41, 30);
+            this.N4.TabIndex = 2;
+            this.N4.Text = "4";
+            this.N4.UseVisualStyleBackColor = false;
+            this.N4.Click += new System.EventHandler(this.N4_Click);
+            // 
+            // N2
+            // 
+            this.N2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.N2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.N2.Location = new System.Drawing.Point(95, 87);
+            this.N2.Name = "N2";
+            this.N2.Size = new System.Drawing.Size(41, 30);
+            this.N2.TabIndex = 3;
+            this.N2.Text = "2";
+            this.N2.UseVisualStyleBackColor = false;
+            this.N2.Click += new System.EventHandler(this.N2_Click);
+            // 
+            // N3
+            // 
+            this.N3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.N3.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.N3.Location = new System.Drawing.Point(165, 87);
+            this.N3.Name = "N3";
+            this.N3.Size = new System.Drawing.Size(41, 30);
+            this.N3.TabIndex = 4;
+            this.N3.Text = "3";
+            this.N3.UseVisualStyleBackColor = false;
+            this.N3.Click += new System.EventHandler(this.N3_Click);
+            // 
+            // N5
+            // 
+            this.N5.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.N5.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.N5.Location = new System.Drawing.Point(95, 123);
+            this.N5.Name = "N5";
+            this.N5.Size = new System.Drawing.Size(41, 30);
+            this.N5.TabIndex = 5;
+            this.N5.Text = "5";
+            this.N5.UseVisualStyleBackColor = false;
+            this.N5.Click += new System.EventHandler(this.N5_Click);
+            // 
+            // N6
+            // 
+            this.N6.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.N6.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.N6.Location = new System.Drawing.Point(165, 123);
+            this.N6.Name = "N6";
+            this.N6.Size = new System.Drawing.Size(41, 30);
+            this.N6.TabIndex = 6;
+            this.N6.Text = "6";
+            this.N6.UseVisualStyleBackColor = false;
+            this.N6.Click += new System.EventHandler(this.N6_Click);
+            // 
+            // N7
+            // 
+            this.N7.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.N7.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.N7.Location = new System.Drawing.Point(29, 159);
+            this.N7.Name = "N7";
+            this.N7.Size = new System.Drawing.Size(41, 30);
+            this.N7.TabIndex = 7;
+            this.N7.Text = "7";
+            this.N7.UseVisualStyleBackColor = false;
+            this.N7.Click += new System.EventHandler(this.N7_Click);
+            // 
+            // N8
+            // 
+            this.N8.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.N8.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.N8.Location = new System.Drawing.Point(95, 159);
+            this.N8.Name = "N8";
+            this.N8.Size = new System.Drawing.Size(41, 30);
+            this.N8.TabIndex = 8;
+            this.N8.Text = "8";
+            this.N8.UseVisualStyleBackColor = false;
+            this.N8.Click += new System.EventHandler(this.N8_Click);
+            // 
+            // N9
+            // 
+            this.N9.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.N9.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.N9.Location = new System.Drawing.Point(165, 159);
+            this.N9.Name = "N9";
+            this.N9.Size = new System.Drawing.Size(41, 30);
+            this.N9.TabIndex = 9;
+            this.N9.Text = "9";
+            this.N9.UseVisualStyleBackColor = false;
+            this.N9.Click += new System.EventHandler(this.N9_Click);
+            // 
+            // N0
+            // 
+            this.N0.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.N0.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.N0.Location = new System.Drawing.Point(95, 195);
+            this.N0.Name = "N0";
+            this.N0.Size = new System.Drawing.Size(41, 30);
+            this.N0.TabIndex = 10;
+            this.N0.Text = "0";
+            this.N0.UseVisualStyleBackColor = false;
+            this.N0.Click += new System.EventHandler(this.N0_Click);
+            // 
+            // bc
+            // 
+            this.bc.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.bc.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bc.Location = new System.Drawing.Point(165, 195);
+            this.bc.Name = "bc";
+            this.bc.Size = new System.Drawing.Size(41, 30);
+            this.bc.TabIndex = 11;
+            this.bc.Text = "C";
+            this.bc.UseVisualStyleBackColor = false;
+            this.bc.Click += new System.EventHandler(this.bc_Click);
+            // 
+            // bdec
+            // 
+            this.bdec.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.bdec.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bdec.Location = new System.Drawing.Point(29, 195);
+            this.bdec.Name = "bdec";
+            this.bdec.Size = new System.Drawing.Size(41, 30);
+            this.bdec.TabIndex = 12;
+            this.bdec.Text = ".";
+            this.bdec.UseVisualStyleBackColor = false;
+            this.bdec.Click += new System.EventHandler(this.bdec_Click);
+            // 
+            // bad
+            // 
+            this.bad.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.bad.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bad.Location = new System.Drawing.Point(228, 87);
+            this.bad.Name = "bad";
+            this.bad.Size = new System.Drawing.Size(41, 30);
+            this.bad.TabIndex = 13;
+            this.bad.Text = "+";
+            this.bad.UseVisualStyleBackColor = false;
+            this.bad.Click += new System.EventHandler(this.bad_Click);
+            // 
+            // bsub
+            // 
+            this.bsub.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.bsub.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bsub.Location = new System.Drawing.Point(228, 123);
+            this.bsub.Name = "bsub";
+            this.bsub.Size = new System.Drawing.Size(41, 30);
+            this.bsub.TabIndex = 14;
+            this.bsub.Text = "-";
+            this.bsub.UseVisualStyleBackColor = false;
+            this.bsub.Click += new System.EventHandler(this.bsub_Click);
+            // 
+            // bmult
+            // 
+            this.bmult.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.bmult.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bmult.Location = new System.Drawing.Point(228, 159);
+            this.bmult.Name = "bmult";
+            this.bmult.Size = new System.Drawing.Size(41, 30);
+            this.bmult.TabIndex = 15;
+            this.bmult.Text = "*";
+            this.bmult.UseVisualStyleBackColor = false;
+            this.bmult.Click += new System.EventHandler(this.bmult_Click);
+            // 
+            // bdiv
+            // 
+            this.bdiv.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.bdiv.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bdiv.Location = new System.Drawing.Point(228, 195);
+            this.bdiv.Name = "bdiv";
+            this.bdiv.Size = new System.Drawing.Size(41, 30);
+            this.bdiv.TabIndex = 16;
+            this.bdiv.Text = "/";
+            this.bdiv.UseVisualStyleBackColor = false;
+            this.bdiv.Click += new System.EventHandler(this.bdiv_Click);
+            // 
+            // bequal
+            // 
+            this.bequal.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.bequal.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bequal.Location = new System.Drawing.Point(95, 246);
+            this.bequal.Name = "bequal";
+            this.bequal.Size = new System.Drawing.Size(41, 30);
+            this.bequal.TabIndex = 17;
+            this.bequal.Text = "=";
+            this.bequal.UseVisualStyleBackColor = false;
+            this.bequal.Click += new System.EventHandler(this.bequal_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 9.75F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 279);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 15);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Made by Kausthubh";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(307, 295);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bequal);
+            this.Controls.Add(this.bdiv);
+            this.Controls.Add(this.bmult);
+            this.Controls.Add(this.bsub);
+            this.Controls.Add(this.bad);
+            this.Controls.Add(this.bdec);
+            this.Controls.Add(this.bc);
+            this.Controls.Add(this.N0);
+            this.Controls.Add(this.N9);
+            this.Controls.Add(this.N8);
+            this.Controls.Add(this.N7);
+            this.Controls.Add(this.N6);
+            this.Controls.Add(this.N5);
+            this.Controls.Add(this.N3);
+            this.Controls.Add(this.N2);
+            this.Controls.Add(this.N4);
+            this.Controls.Add(this.N1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button N1;
+        private System.Windows.Forms.Button N4;
+        private System.Windows.Forms.Button N2;
+        private System.Windows.Forms.Button N3;
+        private System.Windows.Forms.Button N5;
+        private System.Windows.Forms.Button N6;
+        private System.Windows.Forms.Button N7;
+        private System.Windows.Forms.Button N8;
+        private System.Windows.Forms.Button N9;
+        private System.Windows.Forms.Button N0;
+        private System.Windows.Forms.Button bc;
+        private System.Windows.Forms.Button bdec;
+        private System.Windows.Forms.Button bad;
+        private System.Windows.Forms.Button bsub;
+        private System.Windows.Forms.Button bmult;
+        private System.Windows.Forms.Button bdiv;
+        private System.Windows.Forms.Button bequal;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
